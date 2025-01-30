fbe3ed65aba7c9b3253a2191a8b09eb9c8331973
Aharon Seidman <aharondov1@gmail.com>
2025-01-29 22:04:00 -0500

    added tcp split so have method that can read and write

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use anyhow::Result;
-use std::fmt::{Debug, Display};
+use anyhow::{Context, Result};
 use std::io::Write;
 use std::io::{BufRead, BufReader};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 
 #[derive(Debug)]
 enum Command {
@@ -21,23 +20,32 @@ impl From<&str> for Command {
 fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:2525")?;
 
-    if let Ok((mut socket, addr)) = listener.accept() {
+    if let Ok((mut writer_socket, addr)) = listener.accept() {
         println!("recieved connection from: {}", addr);
-        socket.write_all(b"220 localhost SMTP\r\n")?;
-        let mut buffreader = BufReader::new(&socket);
-        let mut response = String::new();
-        buffreader.read_line(&mut response)?;
-        let response = response.trim();
-
-        let response: Command = response.into();
-
-        match response {
-            Command::HELO(_) => {}
-            Command::Unknown => socket.write_all(b"502 Unrecognized command.\r\n")?,
-        }
-        println!("{:?}", response);
+        let reader_socket = writer_socket.try_clone().context("cloning socket")?;
+        let mut buffreader = BufReader::new(&reader_socket);
+        let mut buffer = String::new();
+        hello(&mut writer_socket, &mut buffreader, &mut buffer)?;
     } else {
         println!("connection failed");
+    }
+    Ok(())
+}
+
+fn hello(
+    socket: &mut TcpStream,
+    buffreader: &mut BufReader<&TcpStream>,
+    buffer: &mut String,
+) -> Result<()> {
+    socket.write_all(b"220 localhost SMTP\r\n")?;
+    buffreader.read_line(buffer)?;
+    let response = buffer.trim();
+
+    let response: Command = response.into();
+
+    match response {
+        Command::HELO(_) => {}
+        Command::Unknown => socket.write_all(b"502 Unrecognized command.\r\n")?,
     }
     Ok(())
 }
