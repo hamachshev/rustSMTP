24d194f8a7d17c3be0780d93c0242ca49432012d
Aharon Seidman <aharondov1@gmail.com>
2025-01-29 20:42:41 -0500

    refactored

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,14 @@ use std::net::TcpListener;
 #[derive(Debug)]
 enum Command {
     HELO(String),
+    Unknown,
 }
 
-impl<'a> TryFrom<&'a str> for Command {
-    type Error = CommandParseError;
-
-    fn try_from(value: &'a str) -> std::result::Result<Self, Self::Error> {
+impl From<&str> for Command {
+    fn from(value: &str) -> Self {
         match value {
-            s if s.starts_with("HELO") => Ok(Command::HELO(s[4..].trim().to_string())),
-            _ => Err(CommandParseError {
-                command: value.to_string(), // need this because the error message must be static
-                                            // because it is escaping the place of the error, ie the main fn is returning
-                                            // Result
-            }),
+            s if s.starts_with("HELO") => Command::HELO(s[4..].trim().to_string()),
+            _ => Self::Unknown,
         }
     }
 }
@@ -29,28 +24,20 @@ fn main() -> Result<()> {
     if let Ok((mut socket, addr)) = listener.accept() {
         println!("recieved connection from: {}", addr);
         socket.write_all(b"220 localhost SMTP\r\n")?;
-        let mut buffreader = BufReader::new(socket);
+        let mut buffreader = BufReader::new(&socket);
         let mut response = String::new();
         buffreader.read_line(&mut response)?;
         let response = response.trim();
 
-        let response: Command = response.try_into()?;
+        let response: Command = response.into();
+
+        match response {
+            Command::HELO(_) => {}
+            Command::Unknown => socket.write_all(b"502 Unrecognized command.\r\n")?,
+        }
         println!("{:?}", response);
     } else {
         println!("connection failed");
     }
     Ok(())
 }
-
-#[derive(Debug)]
-struct CommandParseError {
-    command: String,
-}
-
-impl Display for CommandParseError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Invalid Command: {}", self.command)
-    }
-}
-
-impl std::error::Error for CommandParseError {}
