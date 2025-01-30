66b3bce80c433abdcf218c143a612daa2a961902
Aharon Seidman <aharondov1@gmail.com>
2025-01-30 10:05:36 -0500

    added mail from command

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,16 @@ use std::net::{TcpListener, TcpStream};
 enum Command {
     HELO(String),
     Unknown,
+    MailFrom(String),
 }
 
 impl From<&str> for Command {
     fn from(value: &str) -> Self {
         match value {
-            s if s.starts_with("HELO") => Command::HELO(s[4..].trim().to_string()),
+            s if s.starts_with("HELO ") => Command::HELO(s[4..].trim().to_string()),
+            s if s.starts_with("MAIL FROM:<") && s.ends_with(">") => {
+                Command::MailFrom(s[11..s.len() - 1].trim().to_string())
+            }
             _ => Self::Unknown,
         }
     }
@@ -25,7 +29,15 @@ fn main() -> Result<()> {
         let reader_socket = writer_socket.try_clone().context("cloning socket")?;
         let mut buffreader = BufReader::new(&reader_socket);
         let mut buffer = String::new();
-        hello(&mut writer_socket, &mut buffreader, &mut buffer)?;
+        writer_socket.write_all(b"220 localhost SMTP\r\n")?;
+        loop {
+            let Ok(_) = hello(&mut writer_socket, &mut buffreader, &mut buffer) else {
+                continue;
+            };
+            let Ok(_) = mail_from(&mut writer_socket, &mut buffreader, &mut buffer) else {
+                continue;
+            };
+        }
     } else {
         println!("connection failed");
     }
@@ -37,15 +49,46 @@ fn hello(
     buffreader: &mut BufReader<&TcpStream>,
     buffer: &mut String,
 ) -> Result<()> {
-    socket.write_all(b"220 localhost SMTP\r\n")?;
+    buffreader.read_line(buffer)?;
+    let response = buffer.trim();
+
+    let response: Command = response.into();
+
+    buffer.clear();
+
+    match response {
+        Command::HELO(_) => socket.write_all(b"250 localhost at your service \r\n")?,
+        Command::Unknown => {
+            socket.write_all(b"502 Unrecognized command.\r\n")?;
+            anyhow::bail!("Unrecognized command");
+        }
+        _ => {
+            socket.write_all(b"503 Command out of order. Must do HELO/EHLO first.\r\n")?;
+            anyhow::bail!("Command out of order");
+        }
+    }
+    Ok(())
+}
+fn mail_from(
+    socket: &mut TcpStream,
+    buffreader: &mut BufReader<&TcpStream>,
+    buffer: &mut String,
+) -> Result<()> {
     buffreader.read_line(buffer)?;
     let response = buffer.trim();
 
     let response: Command = response.into();
 
     match response {
-        Command::HELO(_) => {}
-        Command::Unknown => socket.write_all(b"502 Unrecognized command.\r\n")?,
+        Command::MailFrom(_) => socket.write_all(b"250 Ok.\r\n")?, // validate email
+        Command::Unknown => {
+            socket.write_all(b"502 Unrecognized command.\r\n")?;
+            anyhow::bail!("Unrecognized command");
+        }
+        _ => {
+            socket.write_all(b"503 Command out of order.\r\n")?;
+            anyhow::bail!("Command out of order");
+        }
     }
     Ok(())
 }
